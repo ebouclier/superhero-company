a4581681723c65ed0e116f777385a5eb9b07f66c
devilairforce <manu.bouclier@gmail.com>
2019-06-03 00:04:15 -0400

    Modiying SpringBoot Controller Add Angular client

--- src/main/java/com/superhero/managemissions/controller/SuperHeroController.java
+++ src/main/java/com/superhero/managemissions/controller/SuperHeroController.java
@@ -6,6 +6,7 @@ import com.superhero.managemissions.model.SuperHero;
 import com.superhero.managemissions.service.ISuperHeroService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import lombok.Data;
 
 @Data
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @Api(value = "superhero", description = "Operations to manage a SuperHero")
 public class SuperHeroController {
 
